d41b66d327fac2960eb7a9c6ca4df192193a1eef
Mark George B Naval <bongnaval04@gmail.com>
2024-06-26 18:40:47 +0800

    favicon

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -6,10 +6,10 @@ const Layout = ({ children }) => {
     return (
         <>
             <Head>
-                <title>Blinder</title>
+                <title>ByteMe</title>
                 <meta name='description' content='Blinder making it simple for you to build and grow your SaaS applications, or any business idea' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <link rel='icon' href='/favicon.ico' />
+                <link rel='icon' href='/favicon.png' />
             </Head>
             <Navbar />
             <main>{children}</main>
